c664242bbc5e34daf57c4fdfb873304160695aa6
ashutoshgovalkar2908 <ashutoshgovalkar942@gmail.com>
2024-08-04 11:52:56 +0530

    update codes

--- portfolio.js
+++ portfolio.js
@@ -1,18 +1,42 @@
-function openMenu(){
-    let openIcon = document.getElementById('open-icon');
-    let navMenu =document.querySelector(".nav-menu2");
-    let close =document.getElementById('close-icon');
-    navMenu.style.display ="block";
-    openIcon.style.display ="none";
-    close.style.display ="block";
-     
-}
-function closeMenu(){
-    let openIcon = document.getElementById('open-icon');
-    let navMenu =document.querySelector(".nav-menu2");
-    let close =document.getElementById('close-icon');
-    navMenu.style.display ="none";
-    openIcon.style.display ="block";
-    close.style.display ="none";
-     
-}
+// functions for open and close nav-baar in small divices
+
+ document.querySelector("#open-icon").addEventListener('click', ()=>{
+        let openIcon = document.getElementById('open-icon');
+        let navMenu =document.querySelector('.nav-menu2');
+        let close =document.getElementById('close-icon');
+
+        navMenu.classList.add('slider');
+        navMenu.classList.remove('slider2');
+        openIcon.style.display ="none";
+        close.style.display ="block";
+ });
+
+ document.querySelector('#close-icon').addEventListener('click', ()=>{
+        let openIcon = document.getElementById('open-icon');
+        let navMenu =document.querySelector(".nav-menu2");
+        let close =document.getElementById('close-icon');
+
+        navMenu.classList.add('slider2');
+        navMenu.classList.remove('slider');
+        openIcon.style.display ="block";
+        close.style.display ="none";
+ });
+
+//  functions for close nav-baar after clicking on links
+
+ let links = document.querySelectorAll(".links");
+ links.forEach(link => {
+     link.addEventListener('click', ()=>{
+        let navMenu = document.querySelector(".nav-menu2");
+        setTimeout(() => {
+            navMenu.classList.add('slider2');
+            navMenu.classList.remove('slider');
+        }, 200); // Wait for the transition to complete
+    
+        let openIcon = document.getElementById('open-icon');
+        let close = document.getElementById('close-icon');
+        openIcon.style.display = "block";
+        close.style.display = "none";
+     });
+ });
+ 
